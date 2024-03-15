469110ec7e1a5d6cb5589b03231702e9fd1968ff
dal07065 <dal07065@gmail.com>
2024-03-14 21:47:35 -0700

    Fix Pancake glb and Update About Me Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,25 +1,52 @@
-// export default function About() {
-//   return (
-//     <div>
-//       <p>About me</p>
-//     </div>
-//   );
-// }
-
-// const About = () => {
-//   console.log("About page loaded");
-//   return <h1 sx={{ color: "#000000" }}>About</h1>;
-// };
-
-// export default About;
+import { red } from "@mui/material/colors";
+import "./stylesheets/About.css";
 
 export default function About() {
   return (
     <div>
       <div>
         <p>
-          <code>About</code>
+          <code>Hi!</code>
         </p>
+      </div>
+
+      <img src="aboutme.jpg"></img>
+
+      <br></br>
+
+      <div id="paragraphs">
+        <br></br>
+        <div id="normal">My name is</div>
+        <div id="coral">Lina Kang</div>
+        <br></br>
+        <br></br>
+        <div id="normal">
+          I graduated from 
+        </div>
+        <div id="blue">
+        Cal Poly Pomona
+        </div>
+        <div id="normal">
+          in Spring 2024
+        </div>
+        <div id="normal">
+        with B.S. in 
+        </div>
+        <div id="green">
+        Computer Science 
+          </div>
+          <br></br>
+        <br></br>
+        <div id="normal">
+          Currently, I am working as a
+        </div>
+        <div id="purple">Software Engineer</div>
+        <div id="normal">in the R&D team at</div>
+        <div id="blue">EON Reality</div>
+        <br></br>
+        <br></br>
+        <div id="normal">Happy to meet you!</div>
+        <br></br>
       </div>
     </div>
   );
